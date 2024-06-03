5fbd0c6dee3efdd6d3dfefd28b8b143bb5b8928b
Omit.Kr <111570631+OmitK1302@users.noreply.github.com>
2024-06-04 00:20:23 +0530

    Time: 22 ms (67.87%), Space: 22.8 MB (31.34%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& a) 
+    {
+        vector<vector<int>>ans;
+        int n=a.size();
+        if(n==0)
+        return ans;
+        sort(a.begin(),a.end());
+        ans.push_back(a[0]);
+        int i,j=0;
+        for(i=1;i<n;i++)
+        {
+            if(ans[j][1]<a[i][0])
+            {
+                ans.push_back(a[i]);
+                j++;
+            }
+            else
+            {
+                ans[j][0]=min(ans[j][0],a[i][0]);
+                ans[j][1]=max(ans[j][1],a[i][1]);
+            }
+        }
+        return ans;
+    }
+};
