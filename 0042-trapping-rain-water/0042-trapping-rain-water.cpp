38e1605ff344928b118bde2aa547c12a738f1f81
Omit.Kr <111570631+OmitK1302@users.noreply.github.com>
2024-06-04 01:21:58 +0530

    Update 0042-trapping-rain-water.cpp

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -11,36 +11,6 @@ public:
             res += (maxL < maxR) ? (maxL) - height[start++] : maxR - height[end--];
         }
 
-
-        // vector<int>maxLeft;
-        // vector<int>maxRight;
-        // vector<int>minmax;
-        // int res = 0;
-        // int maxL = 0, maxR = 0;
-        // for(int i = 0; i < height.size(); i++){
-        //     if(i == 0){
-        //         maxL = 0;
-        //         maxR = 0;
-        //     }
-        //     else{
-        //         maxL = max(maxL, height[i - 1]);
-        //         maxR = max(maxR, height[height.size() - i]);
-        //     }
-        //     maxLeft.push_back(maxL);
-        //     maxRight.insert(maxRight.begin(), maxR);
-        // }
-
-        // for(int i = 0; i < height.size(); i++){
-        //     minmax.push_back(min(maxLeft[i], maxRight[i]));
-        // }
-
-        // for(int i = 0; i < height.size(); i++){
-        //     int temp = minmax[i] - height[i];
-        //     if(temp > 0){
-        //         res += temp;
-        //     }
-        // }
-
         return res;
     }
 };
