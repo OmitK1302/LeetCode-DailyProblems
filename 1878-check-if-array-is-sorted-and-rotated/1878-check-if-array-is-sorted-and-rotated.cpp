0bc1b043b43a9a58e1543344f3d3345912105c7b
Omit.Kr <111570631+OmitK1302@users.noreply.github.com>
2025-02-03 15:58:31 +0530

    1878-CHECK-IF-ARRAY-IS-SORTED-AND-ROTATED Stats: Time: 0 ms (100%), Space: 11.3 MB (23%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int start = 0;
+        
+        // Method 2:
+        int count = 0;
         for(int i = 0; i < nums.size() - 1; i++){
             if(nums[i] > nums[i + 1]){
-                start = i + 1;
-                break;
+                count++;
             }
         }
-        for(int i = start; i < nums.size() - 1; i++){
-            if(nums[i] > nums[i + 1]) return false;
-        }
 
-        if(start != 0 && nums[0] < nums[nums.size() - 1]) return false;
-        return true;
+        if(nums[nums.size() - 1] > nums[0]) count++;
+
+        return count <= 1;
+
+
+        // Method 1:
+        // int start = 0;
+        // for(int i = 0; i < nums.size() - 1; i++){
+        //     if(nums[i] > nums[i + 1]){
+        //         start = i + 1;
+        //         break;
+        //     }
+        // }
+        // for(int i = start; i < nums.size() - 1; i++){
+        //     if(nums[i] > nums[i + 1]) return false;
+        // }
+
+        // if(start != 0 && nums[0] < nums[nums.size() - 1]) return false;
+        // return true;
     }
 };
