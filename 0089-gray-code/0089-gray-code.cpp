4df64cd9650381533da3ab28f856f5f0ce6f713d
Omit.Kr <111570631+OmitK1302@users.noreply.github.com>
2025-02-04 12:51:42 +0530

    0089-GRAY-CODE Stats: Time: 0 ms (100%), Space: 14.8 MB (37.03%) - LeetHub

--- 0089-gray-code/0089-gray-code.cpp
+++ 0089-gray-code/0089-gray-code.cpp
@@ -2,17 +2,26 @@ class Solution {
 public:
     vector<int> grayCode(int n) {
         vector<int>res;
-        res.push_back(0);
-
-        for(int i = 0; i < n; i++){
-            int temp = pow(2, i);
-            int currSize = res.size();
-
-            for(int j = currSize - 1; j >= 0; j--){
-                res.push_back(temp + res[j]);
-            }
+        int b = pow(2, n);
+        // Method 2: using Bit manipulation
+        for(int i = 0; i < (1<<n); i++){
+            res.push_back((i^(i>>1)));
         }
 
         return res;
+        
+        // Method 1: Using math
+        // res.push_back(0);
+
+        // for(int i = 0; i < n; i++){
+        //     int temp = pow(2, i);
+        //     int currSize = res.size();
+
+        //     for(int j = currSize - 1; j >= 0; j--){
+        //         res.push_back(temp + res[j]);
+        //     }
+        // }
+
+        // return res;
     }
 };
