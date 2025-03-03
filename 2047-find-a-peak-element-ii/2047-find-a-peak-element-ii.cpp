d313429815c01bfda341033977d7c0da082af850
Omit.Kr <111570631+OmitK1302@users.noreply.github.com>
2025-03-03 15:51:32 +0530

    2047-FIND-A-PEAK-ELEMENT-II Stats: Time: 3 ms (13.79%), Space: 50.2 MB (95.48%) - LeetHub

--- 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.cpp
@@ -3,18 +3,48 @@ public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                // bool curr = true;
-                if(i - 1 != -1 && mat[i-1][j] > mat[i][j]) continue;
-                if(i + 1 < n && mat[i+1][j] > mat[i][j]) continue;
-                if(j - 1 != -1 && mat[i][j-1] > mat[i][j]) continue;
-                if(j + 1 < m && mat[i][j+1] > mat[i][j]) continue;
+        
+        int low = 0, high = m-1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            int maxRow = 0;
+            for(int i = 0; i < n; i++){
+                if(mat[i][mid] > mat[maxRow][mid]){
+                    maxRow = i;
+                }
+            }
 
-                return {i, j};
+            int left = mid - 1 >= 0 ? mat[maxRow][mid - 1] : -1;
+            int right = mid + 1 < m ? mat[maxRow][mid + 1] : -1;
+            
+            if(mat[maxRow][mid] > left && mat[maxRow][mid] > right){
+                return {maxRow, mid};
+            }
+            else if(mat[maxRow][mid]  < left){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
             }
         }
 
-        return {0, 0};
+        return {-1, -1};
+
+
+
+        // METHOD 1:
+        // for(int i = 0; i < n; i++){
+        //     for(int j = 0; j < m; j++){
+        //         // bool curr = true;
+        //         if(i - 1 != -1 && mat[i-1][j] > mat[i][j]) continue;
+        //         if(i + 1 < n && mat[i+1][j] > mat[i][j]) continue;
+        //         if(j - 1 != -1 && mat[i][j-1] > mat[i][j]) continue;
+        //         if(j + 1 < m && mat[i][j+1] > mat[i][j]) continue;
+
+        //         return {i, j};
+        //     }
+        // }
+
+        // return {0, 0};
     }
 };
