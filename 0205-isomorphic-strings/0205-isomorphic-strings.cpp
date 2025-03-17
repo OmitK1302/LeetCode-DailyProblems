a048eafd029357fdb6790ac67063072bc921836d
Omit.Kr <111570631+OmitK1302@users.noreply.github.com>
2025-03-17 15:45:56 +0530

    0205-ISOMORPHIC-STRINGS Stats: Time: 0 ms (100%), Space: 9.4 MB (54.91%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,49 +1,31 @@
 class Solution {
-public:
-
-    bool containVal(char ch, unordered_map<char, char>map){
-        for(const auto &pair : map){
-            if(pair.second == ch){
-                return true;
-            }
-        }
-        return false;
-    }   
-
-
+public: 
     bool isIsomorphic(string s, string t) {
-        // string ans = "";
         // unordered_map<char, char>map;
-        
-        // if(s.length() != t.length()){
-        //     return false;
-        // }
-        // for(int i = 0; i < s.length(); i++){
-        //     if(!(map.contains(s[i]) || containVal(t[i], map)) ){
-        //         map[s[i]] = t[i];
-        //     }
-        //     ans += map[s[i]];
-        // }
+        // unordered_map<char, int>container;
 
-        // if(ans == t){
-        //     return true;
-        // }
-        // else return false;
-
-
-        unordered_map<char, char>map;
-        unordered_map<char, int>container;
+        vector<char>map(256, '\0');
+        vector<int>container(256, -1);
 
         string res;
         for(int i = 0; i < s.length(); i++){
-            if(!map.count(s[i])){
-                if(container.count(t[i])) return false;
-                map[s[i]] = t[i];
-                container[t[i]] = 1;
+            if(map[(int)s[i]] == '\0'){
+                if(container[(int)t[i]] != -1) return false;
+                map[(int)s[i]] = t[i];
+                container[(int)t[i]] = 1;
             }
 
             res.push_back(map[s[i]]);
         }
+        // for(int i = 0; i < s.length(); i++){
+        //     if(!map.count(s[i])){
+        //         if(container.count(t[i])) return false;
+        //         map[s[i]] = t[i];
+        //         container[t[i]] = 1;
+        //     }
+
+        //     res.push_back(map[s[i]]);
+        // }
 
         return res == t;
     }
