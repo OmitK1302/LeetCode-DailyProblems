192abd5e50e70530b6a40ba1aba9d2fbccb430be
Omit.Kr <111570631+OmitK1302@users.noreply.github.com>
2025-03-21 10:31:55 +0530

    0118-PASCALS-TRIANGLE Stats: Time: 0 ms (100%), Space: 9.6 MB (89.68%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>>res;
-        res.push_back({1});
-        res.push_back({1,1});
-        if(numRows == 1){
-            return {{1}};
-        }
-        else if(numRows == 2){
-            return {{1}, {1,1}};
-        }
+        // if(numRows == 1){
+        //     return {{1}};
+        // }
+        // else if(numRows == 2){
+        //     return {{1}, {1,1}};
+        // }
 
-        for(int i = 3; i <= numRows ; i++){
-            vector<int>temp(i, 1);
-            for(int j = 1; j < i-1; j++){
+        vector<vector<int>>res;
+        // res.push_back({1});
+        // res.push_back({1,1});
+
+        for(int i = 0; i < numRows ; i++){
+            vector<int>temp(i + 1, 1);
+            for(int j = 1; j < i; j++){
                 
                 // else{
-                temp[j] = res[i-2][j] + res[i-2][j-1];
+                temp[j] = res[i-1][j] + res[i-1][j-1];
                 
             }
             res.push_back(temp);
