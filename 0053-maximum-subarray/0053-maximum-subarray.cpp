39cc1398cbf03ba78022eda098547339b3255d01
Omit.Kr <111570631+OmitK1302@users.noreply.github.com>
2025-03-21 14:45:46 +0530

    0053-MAXIMUM-SUBARRAY Stats: Time: 84 ms (5.06%), Space: 71.6 MB (81.09%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        int res = INT_MIN;
         int sum = 0;
-        int res = nums[0];
-        // int negRes = 0;
         for(int& num : nums){
-            if(sum + num <= 0){
-                res = max(res, num);
-                sum = 0;
-                continue;
-            }
             sum += num;
-            // if(sum < 0){
-            //     negRes = min(abs(negRes), abs(res));
-            // }
             res = max(res, sum);
+
+            if(sum < 0){
+                sum = 0;
+                // continue;
+            }
         }
+
         return res;
     }
 };
