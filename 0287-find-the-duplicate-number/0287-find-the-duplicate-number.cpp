32ed8d2c25659ab85d7da9388e44d30e28473c16
Omit.Kr <111570631+OmitK1302@users.noreply.github.com>
2025-03-25 11:00:24 +0530

    0287-FIND-THE-DUPLICATE-NUMBER Stats: Time: 4 ms (61.42%), Space: 67.6 MB (22.16%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-
-        for(int i = 0; i < nums.size() - 1; i++){
-            if(nums[i] == nums[i+1]){
-                return nums[i];
-            }
+        vector<int>freq(nums.size() - 1);
+        for(int& num : nums){
+            if(freq[num - 1] == 1) return num;
+            freq[num - 1] = 1;
         }
 
         return -1;
+
+
+        // O(nlongn) solution without extra space;
+        // sort(nums.begin(), nums.end());
+
+        // for(int i = 0; i < nums.size() - 1; i++){
+        //     if(nums[i] == nums[i+1]){
+        //         return nums[i];
+        //     }
+        // }
+
+        // return -1;
     }
 };
