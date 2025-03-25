afa92118441cfb14d970cb164ee90de841e10c43
Omit.Kr <111570631+OmitK1302@users.noreply.github.com>
2025-03-25 09:43:09 +0530

    0048-ROTATE-IMAGE Stats: Time: 0 ms (100%), Space: 10.3 MB (34.84%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,12 +3,33 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         vector<vector<int>>res(matrix.size(), vector<int>(matrix[0].size()));
 
+        // for(int i = 0; i < matrix.size() / 2; i++){
+        //     int j = i;
+        //     while(j < matrix[0].size() - i){
+        //         for(int k = 0; k < 4; k++){
+
+        //         }
+        //     }
+        // }
+
         for(int i = 0; i < matrix.size(); i++){
-            for(int j = 0; j < matrix[0].size(); j++){
-                res[j][matrix.size() - i - 1] = matrix[i][j];
+            for(int j = i; j < matrix[0].size(); j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+                // res[j][matrix.size() - i - 1] = matrix[i][j];
+            }
+        }
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = 0; j < matrix[0].size() / 2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix[0].size() - j - 1];
+                matrix[i][matrix[0].size() - j - 1] = temp;
+                // res[j][matrix.size() - i - 1] = matrix[i][j];
             }
         }
 
-        matrix = res;
+
+        // matrix = res;
     }
 };
