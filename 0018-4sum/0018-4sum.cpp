f7a9ee7f34657d5c99d7370493128fb2a2604b7f
Omit.Kr <111570631+OmitK1302@users.noreply.github.com>
2025-04-05 14:29:29 +0530

    0018-4SUM Stats: Time: 171 ms (16.35%), Space: 53.7 MB (8.67%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,53 +1,32 @@
+
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        sort(begin(nums), end(nums));
-        return kSum(nums, target, 0, 4);
-    }
-
-    vector<vector<int>> kSum(vector<int>& nums, long long target, int start, int k) {
-        vector<vector<int>> res;
-        if (start == nums.size()) {
-            return res;
-        }
-        long long average_value = target / k;
-        if (nums[start] > average_value || average_value > nums.back()) {
-            return res;
-        }
-
-        if (k == 2) {
-            return twoSum(nums, target, start);
-        }
-
-        for (int i = start; i < nums.size(); ++i) {
-            if (i == start || nums[i - 1] != nums[i]) {
-                vector<vector<int>>temp = kSum(nums, static_cast<long long>(target) - nums[i], i + 1, k - 1);
-                for (vector<int>& subset : temp) {
-                    res.push_back({nums[i]});
-                    res.back().insert(end(res.back()), begin(subset), end(subset));
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        set<vector<int>> set;
+        vector<vector<int>> output;
+        for(int i=0; i<n-3; i++){
+            for(int j=i+1; j<n-2; j++){
+                long long newTarget = (long long)target - (long long)nums[i] - (long long)nums[j];
+                int low = j+1, high = n-1;
+                while(low < high){
+                    if(nums[low] + nums[high] < newTarget){
+                        low++;
+                    }
+                    else if(nums[low] + nums[high] > newTarget){
+                        high--;
+                    }
+                    else{
+                        set.insert({nums[i], nums[j], nums[low], nums[high]});
+                        low++; high--;
+                    }
                 }
             }
         }
-
-        return res;
-    }
-
-    vector<vector<int>> twoSum(vector<int>& nums, long long target, int start) {
-        vector<vector<int>> res;
-        int low = start, high = int(nums.size()) - 1;
-
-        while (low < high) {
-            int curr_sum = nums[low] + nums[high];
-            if (curr_sum < target || (low > start && nums[low] == nums[low - 1])) {
-                low++;
-            } else if (curr_sum > target ||
-                       (high < nums.size() - 1 && nums[high] == nums[high + 1])) {
-                high--;
-            } else {
-                res.push_back({nums[low++], nums[high--]});
-            }
+        for(auto it : set){
+            output.push_back(it);
         }
-
-        return res;
+        return output;
     }
 };
