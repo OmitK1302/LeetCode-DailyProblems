25da48bc89d46f7017ff7b414767d8523978d9a8
Omit.Kr <111570631+OmitK1302@users.noreply.github.com>
2025-07-16 14:23:26 +0530

    0503-NEXT-GREATER-ELEMENT-II Stats: Time: 2 ms (80.04%), Space: 29 MB (15.65%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,34 +1,46 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        // unordered_map<int, int>map;
-
-        // for(int i = 0; i < nums.size(); i++) {
-        //     map[nums[i]] = i;
-        // }
-
-        vector<int>res(nums.size());
-
-        int j = 0;
+        // Method 1---------------------------------------------------------------------------
+        stack<int> st;
         int n = nums.size();
-        for(int i = 0; i < n; i++) {
-            bool flag = false;
-            int val1 = nums[i];
-            for(int j = 0; j < nums.size(); j++) {
-                int indx = (i + j) % n;
-                if(nums[indx] > val1) {
-                    res[i] = nums[indx];
-                    flag = true;
-                    break;
-                }
-            }    
-            if(!flag)
-                res[i] = -1;
+        for(int i = n-1; i >= 0; i--) {
+            st.push(nums[i]);
+        }
 
+        vector<int> res(n, -1);
+        for(int i = n-1; i >= 0; i--) {
+            while(!st.empty() && st.top() <= nums[i]) {
+                st.pop();
+            }
 
-            
+            if(!st.empty()) {
+                res[i] = st.top();
+            }
+            st.push(nums[i]);
         }
 
         return res;
+
+
+        // Method 2---------------------------------------------------------------------------
+        // vector<int>res(nums.size());
+        // int n = nums.size();
+        // for(int i = 0; i < n; i++) {
+        //     bool flag = false;
+        //     int val1 = nums[i];
+        //     for(int j = 0; j < nums.size(); j++) {
+        //         int indx = (i + j) % n;
+        //         if(nums[indx] > val1) {
+        //             res[i] = nums[indx];
+        //             flag = true;
+        //             break;
+        //         }
+        //     }    
+        //     if(!flag)
+        //         res[i] = -1;
+        // }
+
+        // return res;
     }
 };
