7638ce959c59c1a15259a676e55c1a9c6c9176ad
Omit.Kr <111570631+OmitK1302@users.noreply.github.com>
2025-07-17 15:02:34 +0530

    0307-RANGE-SUM-QUERY-MUTABLE Stats: Time: 53 ms (74.52%), Space: 182.2 MB (54.33%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
@@ -4,68 +4,62 @@ public:
     int n;
     NumArray(vector<int>& nums) {
         n = nums.size();
-        seg.resize(4*n + 1, 0);
+        seg.resize(4*n, 0);
 
-        buildTree(0, nums, 0, n-1);
-
-        // for(int& num : seg) {
-        //     cout<<num<<" ";
-        // }
-        // cout<<endl;
+        buildSeg(0, nums, 0, n-1);
     }
     
     void update(int index, int val) {
-        updateTree(index, val, 0, 0, n-1);
-
-        // for(int& num : seg) {
-        //     cout<<num<<" ";
-        // }
-        // cout<<endl;
+        updateSeg(index, val, 0, 0, n-1);
     }
     
     int sumRange(int left, int right) {
-        return query(left, right, 0, n-1, 0);
+        return querySeg(left, right, 0, 0, n-1);
     }
 
 
-    int buildTree(int i, vector<int>& nums, int start, int end) {
+    // Made functions:
+    int buildSeg(int i, vector<int>& nums, int start, int end) {
         if(start == end) {
             seg[i] = nums[start];
-            return nums[start];
+            return seg[i];
         }
 
         int mid = (start + end) / 2;
-        int ls = buildTree(2*i + 1, nums, start, mid);
-        int rs = buildTree(2*i + 2, nums, mid + 1, end);
+        int ls = buildSeg(2*i + 1, nums, start, mid);
+        int rs = buildSeg(2*i + 2, nums, mid + 1, end);
 
         return seg[i] = ls + rs;
     }
 
-    void updateTree(int indx, int val, int i, int start, int end) {
+    void updateSeg(int indx, int val, int i, int start, int end) {
         if(start == end) {
             seg[i] = val;
             return;
         }
+
         int mid = (start + end) / 2;
         if(indx <= mid) {
-            updateTree(indx, val, 2*i + 1, start, mid);
+            updateSeg(indx, val, 2*i + 1, start, mid);
         }
         else{
-            updateTree(indx, val, 2*i + 2, mid + 1, end);
+            updateSeg(indx, val, 2*i + 2, mid + 1, end);
         }
 
         seg[i] = seg[2*i + 1] + seg[2*i + 2];
     }
 
-    int query(int l, int r, int start, int end, int i) {
+    int querySeg(int l, int r, int i, int start, int end) {
         if(start >= l && end <= r) {
             return seg[i];
         }
-        else if(l > end || r < start) {
+        else if(end < l || start > r) {
             return 0;
         }
+
         int mid = (start + end) / 2;
-        return query(l, r, start, mid, 2*i + 1) + query(l, r, mid + 1, end, 2*i + 2);
+
+        return querySeg(l, r, 2*i + 1, start, mid) + querySeg(l, r, 2*i + 2, mid + 1, end);
     }
 };
 
