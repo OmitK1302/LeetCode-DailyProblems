3858c78e1c4375597ffb6012098c73ac453006cb
Omit.Kr <111570631+OmitK1302@users.noreply.github.com>
2025-07-18 21:44:15 +0530

    0338-COUNTING-BITS Stats: Time: 7 ms (6.1%), Space: 11.3 MB (31.2%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
+
+    int bitCnt(int val) {
+        int res = 0;
+        for(int i = 0; i < 32; i++) {
+            if(((val >> i) & 1) == 1) {
+                res++;
+            }
+        }
+
+        return res;
+    }
     vector<int> countBits(int n) {
         vector<int>res;
         for(int i = 0; i <= n; i++) {
-            res.push_back(__builtin_popcount(i));
+            res.push_back(bitCnt(i));
         }
         return res;
     }
