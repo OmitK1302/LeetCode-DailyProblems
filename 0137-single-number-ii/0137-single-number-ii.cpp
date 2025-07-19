a95a7523feab3da75c7cb44a57a2f3ca14cc9e4d
Omit.Kr <111570631+OmitK1302@users.noreply.github.com>
2025-07-20 02:19:56 +0530

    0137-SINGLE-NUMBER-II Stats: Time: 2 ms (39.05%), Space: 13.3 MB (64.59%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int result = 0;
-        for(int i = 0; i < 32; i++){
-            int count0 = 0;
-            int count1 = 0;
-            int temp = (1 << i);
-            for(int& num : nums){
-                if((num & temp) != 0) count1++;
-                else count0++;
+        int ans = 0;
+        for(int i = 0; i < 32; i++) {
+            int temp = 0;
+            for(int& num : nums) {
+                if(num & (1 << i)) {
+                    temp++;
+                }
             }
 
-            if(count1 % 3 == 1){
-                result |= temp;
+            if(temp % 3) {
+                ans |= (1 << i);
             }
         }
-
-        return result;
+        return ans;
     }
 };
