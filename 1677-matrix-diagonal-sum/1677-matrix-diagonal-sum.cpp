2f1c396705f4142e8c37de8defaf987e0b3b6205
Omit.Kr <111570631+OmitK1302@users.noreply.github.com>
2025-07-23 23:54:57 +0530

    1677-MATRIX-DIAGONAL-SUM Stats: Time: 0 ms (100%), Space: 15 MB (53.51%) - LeetHub

--- 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.cpp
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.cpp
@@ -1,33 +1,15 @@
 class Solution {
 public:
-    int diagonalSum(vector<vector<int>>& matrix) {
-        // Code here
-        int sum1 = 0;
-        // int sum2;
-        for(int i = 0; i < matrix.size(); i++){
-            for(int j = 0; j < matrix[i].size(); j++){
-                if(i == j){
-                    sum1 +=  matrix[i][j];
-                }
-            }
+    int diagonalSum(vector<vector<int>>& mat) {
+        int sum = 0;
+        for(int i = 0; i < mat.size(); i++) {
+            sum += mat[i][i];
         }
-        // cout<<sum1<<" ";
-        
-        
-        int sum2 = 0;
-        for(int i = 0; i < matrix.size(); i++){
-            for(int j = 0; j < matrix[i].size(); j++){
-                if(j == matrix[i].size() - 1){
-                    if(i == j-i) continue;
-                    sum2 += matrix[i][j-i];
-                }
-                
-            }
+
+        for(int i = 0; i < mat.size(); i++) {
+            if(i == mat.size() - i -1) continue;
+            sum += mat[i][mat.size() - i -1];
         }
-        // cout<<sum2<<" ";
-        
-        int result = sum1+ sum2;
-        int n = matrix.size();
-        return result;
+        return sum;
     }
 };
