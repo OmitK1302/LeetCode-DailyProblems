4f14121bc2938a3ba5fc9e85db26cf6a02853c7c
Omit.Kr <111570631+OmitK1302@users.noreply.github.com>
2025-07-27 00:05:02 +0530

    2271-REARRANGE-ARRAY-ELEMENTS-BY-SIGN Stats: Time: 10 ms (36.19%), Space: 128.3 MB (79.46%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -1,26 +1,44 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int>pos;
-        vector<int>neg;
+        // vector<int>pos;
+        // vector<int>neg;
+
+        // for(int& num : nums) {
+        //     if(num > 0) {
+        //         pos.push_back(num);
+        //     }
+        //     else{
+        //         neg.push_back(num);
+        //     }
+        // }
+        // // return neg;
+        // // return pos;
+
+        // int j = 0;
+        // for(int i = 0; i < nums.size(); i+=2) {
+        //     nums[i] = pos[j];
+        //     nums[i+1] = neg[j];
+        //     j++;
+        // }
+        // return nums;
+
+
+        int posIn = 0;
+        int negIn = 1;
+
+        vector<int>res(nums.size());
 
         for(int& num : nums) {
             if(num > 0) {
-                pos.push_back(num);
+                res[posIn] = num;
+                posIn += 2;
             }
             else{
-                neg.push_back(num);
+                res[negIn] = num;
+                negIn += 2;
             }
         }
-        // return neg;
-        // return pos;
-
-        int j = 0;
-        for(int i = 0; i < nums.size(); i+=2) {
-            nums[i] = pos[j];
-            nums[i+1] = neg[j];
-            j++;
-        }
-        return nums;
+        return res;
     }
 };
