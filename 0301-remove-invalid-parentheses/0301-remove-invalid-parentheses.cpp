3a6949b101dd99b740984f3b4cc5a2e3bfd232a7
Omit.Kr <111570631+OmitK1302@users.noreply.github.com>
2025-07-28 15:40:21 +0530

    0301-REMOVE-INVALID-PARENTHESES Stats: Time: 67 ms (61.23%), Space: 19.5 MB (57.61%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
@@ -3,7 +3,7 @@ public:
     unordered_set<string> res;
     unordered_set<string> visited;
 
-    bool isValid(const string& s) {
+    bool isValid(string& s) {
         int bal = 0;
         for (char ch : s) {
             if (ch == '(') bal++;
@@ -46,9 +46,28 @@ public:
         }
     }
 
+    // void helper(string s, int i, int cnt) {
+    //     if(i == s.length()) {
+    //         if(isValid(s)) {
+    //             res.insert(s);
+    //             // res.push_back(s);
+    //         }
+    //         return;
+    //     }
+        
+    //     if(cnt > 0) {
+    //         string temp = s;
+    //         temp.erase(i, 1);
+    //         helper(temp, i, cnt - 1);
+    //     }
+        
+    //     helper(s, i+1, cnt);
+    // }
+
     vector<string> removeInvalidParentheses(string s) {
         int cnt = findMin(s);
         helper(s, cnt);
+        // helper(s, 0, cnt);
         return vector<string>(res.begin(), res.end());
     }
 };
