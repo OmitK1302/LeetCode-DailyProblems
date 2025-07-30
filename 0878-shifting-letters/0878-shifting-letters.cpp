5789ca562320416b35b13cf468a9f24049927c3e
Omit Kumar <111570631+OmitK1302@users.noreply.github.com>
2025-07-30 12:11:50 +0530

    0878-SHIFTING-LETTERS Stats: Time: 4 ms (61.6%), Space: 73.2 MB (43.47%) - LeetHub

--- 0878-shifting-letters/0878-shifting-letters.cpp
+++ 0878-shifting-letters/0878-shifting-letters.cpp
@@ -7,16 +7,15 @@ public:
     }
 
     string shiftingLetters(string s, vector<int>& shifts) {
-        vector<long long>currShift(shifts.size(), 0);
-        currShift[shifts.size() - 1] = shifts[shifts.size() - 1] % 26;
         for(int i = shifts.size() - 2; i >= 0; i--) {
-            currShift[i] = currShift[i+1] + shifts[i] % 26;
+            long long curr = shifts[i];
+            shifts[i] = (curr + shifts[i+1]) % 26;
         }
 
         // return to_string(shift('f', 2));
         string temp;
         for(int i = 0; i < s.length(); i++) {
-            temp += shift(s[i], currShift[i]);
+            temp += shift(s[i], shifts[i]);
         }
         return temp;
         // for(int i)
