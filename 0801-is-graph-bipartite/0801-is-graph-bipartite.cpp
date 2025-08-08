d5a66ef543f9346799fd6fefe9a726a143eea01b
Omit Kumar <111570631+OmitK1302@users.noreply.github.com>
2025-08-08 09:22:36 +0530

    0801-IS-GRAPH-BIPARTITE Stats: Time: 0 ms (100%), Space: 17.5 MB (38.13%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,27 +1,39 @@
 class Solution {
 public:
-    bool helperDFS(vector<vector<int>>& graph, int currNode, int currColor, vector<int>& color){
-        color[currNode] = currColor;
-        for(int& i : graph[currNode]){
-            if(color[i] == color[currNode]) return false;
+    bool helperBfs(vector<vector<int>>& graph, vector<int>& color, int src) {
+        int clr = 0;
+        
+        queue<int>que;
+        que.push(src);
 
-            if(color[i] == -1){
-                int colorOfI = 1 - currColor;
-                if(helperDFS(graph, i, colorOfI, color) == false) return false;
+        while(!que.empty()) {
+            clr = clr == 0 ? 1 : 0;
+            int n = que.size();
+            for(int i = 0; i < n; i++) {
+                int curr = que.front();
+                que.pop();
+
+                for(auto& node : graph[curr]) {
+                    if(color[node] != -1) {
+                        if(color[node] != clr) return false;
+                    }
+                    else{
+                        color[node] = clr;
+                        que.push(node);
+                    }
+                }
             }
         }
 
         return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int>color(n, -1);
-
-        for(int i = 0; i < n; i++){
-            if(color[i] == -1)
-                if(helperDFS(graph, i, 1, color) == false) return false;
+        vector<int>color(graph.size(), -1);
+        for(int i = 0; i < graph.size(); i++) {
+            if(color[i] == -1) {
+                if(!helperBfs(graph, color, i)) return false;
+            }
         }
-
         return true;
     }
 };
