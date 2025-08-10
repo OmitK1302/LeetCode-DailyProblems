6e7b7871f60a21cdb481e2454c97a73b2c90463b
Omit Kumar <111570631+OmitK1302@users.noreply.github.com>
2025-08-10 15:12:00 +0530

    0900-REORDERED-POWER-OF-2 Stats: Time: 0 ms (100%), Space: 9 MB (51.03%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
     bool reorderedPowerOf2(int n) {
-        vector<string>powerOf2 = { "1", "2", "4", "8", "16", "23", "46", "128", "256", "125",
-            "0124", "0248", "0469", "1289", "13468", "23678", "35566",
-            "011237", "122446", "224588", "0145678", "0122579", 
-            "0134449", "0368888", "11266777", "23334455", "01466788",
-            "112234778", "234455668", "012356789", "0112344778",
-            "1234446788", "2244667999", "2345588999" };
+        string curr = to_string(n);
+        sort(curr.begin(), curr.end());
 
-        string temp = to_string(n);
-        sort(temp.begin(), temp.end());
-        
-        if(find(powerOf2.begin(), powerOf2.end(), temp) != powerOf2.end()) return true;
+        int res = 0;
+        for(int i = 0; i < 32; i++) {
+            string temp = to_string(1 << i);
+            sort(temp.begin(), temp.end());
+            if(curr == temp) return true;
+        }
+
         return false;
     }
+
+
+
+
+    // bool reorderedPowerOf2(int n) {
+    //     vector<string>powerOf2 = { "1", "2", "4", "8", "16", "23", "46", "128", "256", "125",
+    //         "0124", "0248", "0469", "1289", "13468", "23678", "35566",
+    //         "011237", "122446", "224588", "0145678", "0122579", 
+    //         "0134449", "0368888", "11266777", "23334455", "01466788",
+    //         "112234778", "234455668", "012356789", "0112344778",
+    //         "1234446788", "2244667999", "2345588999" };
+
+    //     string temp = to_string(n);
+    //     sort(temp.begin(), temp.end());
+        
+    //     if(find(powerOf2.begin(), powerOf2.end(), temp) != powerOf2.end()) return true;
+    //     return false;
+    // }
 
 
 
