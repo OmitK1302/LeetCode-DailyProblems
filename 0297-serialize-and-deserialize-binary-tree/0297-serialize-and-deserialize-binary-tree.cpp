d968e6d2cb91f5e9e524da73cb1747c4a199be71
Omit Kumar <111570631+OmitK1302@users.noreply.github.com>
2025-08-19 15:48:43 +0530

    0297-SERIALIZE-AND-DESERIALIZE-BINARY-TREE Stats: Time: 33 ms (27.82%), Space: 32.5 MB (45.38%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
@@ -8,132 +8,72 @@
  * };
  */
 class Codec {
-    TreeNode* construct(istringstream& ss) {
-        string val;
-        ss >> val;
-        if(val == "#") return nullptr;
-
-        TreeNode* root = new TreeNode(stoi(val));
-        root -> left = construct(ss); 
-        root -> right = construct(ss); 
-
-        return root;
-    }
 public:
+    // string res;
+    string serialize(TreeNode *root) {
+        
+        if(root == nullptr) {
+            return "null";
+        }
 
-    // Encodes a tree to a single string.
-    string serialize(TreeNode* root) {
-        if(!root) return "# ";
-        return to_string(root -> val) + " " + serialize(root -> left) + serialize(root -> right);
+        queue<TreeNode*>que;
+        que.push(root);
+
+        string res;
+        
+        while(!que.empty()) {
+            TreeNode* curr = que.front();
+            que.pop();
+            if(curr) {
+                res += (to_string(curr -> val) + ",");
+                que.push(curr -> left);
+                que.push(curr -> right);
+            }
+            else{
+                res += "null,";
+            }
+        }
+
+        res.pop_back();
+        return res;
     }
 
-    // Decodes your encoded data to tree.
-    TreeNode* deserialize(string data) {
-        istringstream ss(data);
-        cout<<data;
-        return construct(ss);
-        return nullptr;
+    TreeNode *deserialize(string res) {
+        if(res == "null") return nullptr;
+        // cout<<res<<" ";
+        stringstream ss(res);
+        string token;
+        
+        getline(ss, token, ',');
+        cout<<token<<" "<<endl;
+        if(token == "null" || token.empty()) return nullptr;
+        TreeNode* root = new TreeNode(stoi(token));
+        queue<TreeNode*>que;
+        que.push(root);
+        
+        while(!que.empty()) {
+            TreeNode* curr = que.front();
+            que.pop();
+            
+            if(!getline(ss, token, ',')) break;
+            // cout<<token<<" ";
+            if(token != "null" && !token.empty()) {
+                curr -> left = new TreeNode(stoi(token));
+                que.push(curr -> left);
+            }
+            
+            if(!getline(ss, token, ',')) break;
+            // cout<<token<<" ";
+            if(token != "null" && !token.empty()) {
+                curr -> right = new TreeNode(stoi(token));
+                que.push(curr -> right);
+            }
+        }
+        
+        return root;
     }
 };
 
 // Your Codec object will be instantiated and called as such:
 // Codec ser, deser;
 // TreeNode* ans = deser.deserialize(ser.serialize(root));
-
-
-
-
-// ------------------------------------------------------------------------------------------------------------------------------------------------
-
-// This method will not work for duplicate node values
-
-/*
-class Codec {
-    void preNode(TreeNode* root, vector<int>&pre) {
-        if(!root) return ;
-
-        pre.push_back(root -> val);
-        preNode(root -> left, pre);
-        preNode(root -> right, pre);
-    }
-
-    void inNode(TreeNode* root, vector<int>&in) {
-        if(!root) return ;
-
-        inNode(root -> left, in);
-        in.push_back(root -> val);
-        inNode(root -> right, in);
-    }
-
-    vector<int> order(string data, int start, int end) {
-        vector<int>res;
-        for(int i = start; i < end; i++) {
-            string temp;
-            while(i < end && data[i] != ' ') {
-                temp += data[i++];
-            }
-            res.push_back(stoi(temp));
-        }
-        return res;
-    }
-
-    TreeNode* construct(vector<int>& pre, unordered_map<int, int>& mp, int& indx, int start, int end) {
-        if(start > end) return nullptr;
-        TreeNode* root = new TreeNode(pre[indx++]);
-        int mid = mp[root -> val];
-
-        root -> left = construct(pre, mp, indx, start, mid - 1);
-        root -> right = construct(pre, mp, indx, mid + 1, end);
-
-        return root;
-    }
-
-public:
-
-    // Encodes a tree to a single string.
-    string serialize(TreeNode* root) {
-        vector<int>pre;
-        vector<int>in;
-
-        preNode(root, pre);
-        inNode(root, in);
-
-        string str;
-        int n = pre.size();
-        for(int& nd : pre) {
-            str += to_string(nd) + " ";
-        }
-
-        for(int& nd : in) {
-            str += to_string(nd) + " ";
-        }
-
-        // cout<<str;
-        return str;
-    }
-
-    // Decodes your encoded data to tree.
-    TreeNode* deserialize(string data) {
-        int n = data.length();
-        vector<int>pre = order(data, 0, n/2);
-        vector<int>in = order(data, n/2, n);
-
-        unordered_map<int, int>mp;
-        for(int i = 0; i < in.size(); i++) {
-            mp[in[i]] = i;
-        }
-
-        for(int& val : pre) {
-            cout<<val<<" ";
-        }
-        cout<<endl;
-        
-        int indx = 0;
-        int len = in.size();
-        return construct(pre, mp, indx, 0, len - 1);
-        // return nullptr;
-
-    }
-};
-
-*/
