61d7b95d8d2b4860b265028a0ed33f8bdad9f287
Omit Kumar <111570631+OmitK1302@users.noreply.github.com>
2025-08-25 00:58:38 +0530

    0371-SUM-OF-TWO-INTEGERS Stats: Time: 0 ms (100%), Space: 7.6 MB (94.6%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,6 +1,36 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return a + b;
+        int res = 0;
+        int carry = 0;
+        for(int i = 0; i < 32; i++) {
+            int first = (1 << i) & a;
+            int sec = (1 << i) & b;
+
+            if(first && sec) {
+                if(carry == 1) {
+                    res |= (1 << i);
+                }
+                else{
+                    carry = 1;
+                }
+            }
+
+            else if(!first && !sec) {
+                if(carry == 1) {
+                    res |= (1 << i);
+                    carry = 0;
+                }
+            }
+
+            else{
+                if(!carry) {
+                    res |= (1 << i);
+                }
+            }
+        }
+
+
+        return res;
     }
 };
