8ba856bf8668ce98c5ab9e271d2f2fec12f204ea
Omit Kumar <111570631+OmitK1302@users.noreply.github.com>
2025-08-25 10:49:10 +0530

    0943-SUM-OF-SUBARRAY-MINIMUMS Stats: Time: 1978 ms (5.02%), Space: 76.8 MB (5%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -1,35 +1,64 @@
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
-        int n = arr.size();
         int MOD = 1e9 + 7;
+        unordered_map<int ,int>mp1;
+        unordered_map<int ,int>mp2;
+        stack<int>st1;
+        stack<int>st2;
         long long res = 0;
+        int n = arr.size();
+        vector<long long> helper(n, 1);
+
+        // return 1;
         for(int i = 0; i < arr.size(); i++) {
-            int leftMin = -1;
-            int rightMin = n;
+            // cout<<arr[i]<<": ";
+            while(!st1.empty() && st1.top() > arr[i]) {
+                // cout<<st.top()<<" ";
+                mp1.erase(st1.top());
+                st1.pop();
+            }
+            while(!st2.empty() && st2.top() >= arr[n-i-1]) {
+                // cout<<st.top()<<" ";
+                mp2.erase(st2.top());
+                st2.pop();
+            }
+            // cout<<endl;
 
-            for(int j = i-1; j >= 0; j--) {
-                if(arr[j] < arr[i]) {
-                    leftMin = j;
-                    break;
-                }
+            int left = 1;
+            int right = 1;
+
+            if(st1.empty()) {
+                // res = (res + (i * arr[i])) % MOD;
+                left = i+1;
+            }
+            else{
+                // res = (res + ((i - mp[st.top()] - 1) * arr[i])) % MOD;
+                left = i - mp1[st1.top()];
             }
 
-            for(int j = i+1; j < n; j++) {
-                if(arr[j] <= arr[i]) {
-                    rightMin = j;
-                    break;
-                }
+            if(st2.empty()) {
+                right = i + 1;
+            }
+            else{
+                right = mp2[st2.top()] - (n - i - 1);
             }
 
-            long long left = i - leftMin;
-            long long right = rightMin - i;
-
-            res = (res + (1LL * arr[i] * left % MOD * right % MOD)) % MOD;
-
-            // res += ((((arr[i] * (left) % MOD) * (rightMin - i)) % MOD) % MOD);
+            cout<<left<<" "<<right<<endl;
+            // res += (left * right * arr)
+            helper[i] *= left; 
+            helper[n-i-1] *= right; 
+            st1.push(arr[i]);
+            mp1[arr[i]] = i;
+            st2.push(arr[n-i-1]);
+            mp2[arr[n-i-1]] = n-i-1;
         }
 
-        return res;
+
+        for(int i = 0; i < helper.size(); i++) {
+            cout<<helper[i]<<" ";
+            res += ((helper[i] * arr[i]) % MOD);
+        }
+        return res % MOD;
     }
 };
