7aa4a1325f1c74d474234973d81bdbca6f5ffad9
Omit Kumar <111570631+OmitK1302@users.noreply.github.com>
2025-08-27 11:43:44 +0530

    3733-LENGTH-OF-LONGEST-V-SHAPED-DIAGONAL-SEGMENT Stats: Time: 0 ms (100%), Space: 588 MB (10.69%) - LeetHub

--- 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.cpp
+++ 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.cpp
@@ -56,6 +56,7 @@
 
 // };
 
+
 #pragma GCC optimize("O3", "unroll-loops")
 
 const auto _ = std::cin.tie(nullptr)->sync_with_stdio(false);
@@ -69,42 +70,42 @@ const auto __ = []() {
 }();
 #endif
 
+
 class Solution {
 public:
+    int m, n;
+    vector<vector<int>> dirs = {{1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
+    vector<vector<vector<vector<int>>>> dp;
+    int dfs(vector<vector<int>>& grid, int cx, int cy, int dir, bool turn, int target) {
+        int nx = cx + dirs[dir][0];
+        int ny = cy + dirs[dir][1];
+        if (nx < 0 || ny < 0 || nx >= m || ny >= n ||
+            grid[nx][ny] != target) {
+            return 0;
+        }
+        if (dp[nx][ny][dir][turn] != -1) {
+            return dp[nx][ny][dir][turn];
+        }
+
+        int maxStep = dfs(grid, nx, ny, dir, turn, 2 - target);
+        if (turn) {
+            maxStep = max(maxStep, dfs(grid, nx, ny, (dir + 1) % 4, false, 2 - target));
+        }
+        dp[nx][ny][dir][turn] = maxStep + 1;
+        return maxStep + 1;
+    }
+
     int lenOfVDiagonal(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> dirs = {{1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
+        m = grid.size(); n = grid[0].size();
 
-        vector<vector<vector<vector<int>>>> dp(m, vector<vector<vector<int>>>(n, vector<vector<int>>(4, vector<int>(2, -1))));
-        // int dp[m][n][4][2];
-        // memset(dp, -1, sizeof(memo));
-
-        function<int(int, int, int, bool, int)> dfs =
-            [&](int cx, int cy, int direction, bool turn, int target) -> int {
-            int nx = cx + dirs[direction][0];
-            int ny = cy + dirs[direction][1];
-            if (nx < 0 || ny < 0 || nx >= m || ny >= n ||
-                grid[nx][ny] != target) {
-                return 0;
-            }
-            if (dp[nx][ny][direction][turn] != -1) {
-                return dp[nx][ny][direction][turn];
-            }
-
-            int maxStep = dfs(nx, ny, direction, turn, 2 - target);
-            if (turn) {
-                maxStep = fmax(maxStep, dfs(nx, ny, (direction + 1) % 4, false, 2 - target));
-            }
-            dp[nx][ny][direction][turn] = maxStep + 1;
-            return maxStep + 1;
-        };
+        dp.resize(m, vector<vector<vector<int>>>(n, vector<vector<int>>(4, vector<int>(2, -1))));
 
         int res = 0;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (grid[i][j] == 1) {
-                    for (int direction = 0; direction < 4; ++direction) {
-                        res = fmax(res, dfs(i, j, direction, true, 2) + 1);
+                    for (int dir = 0; dir < 4; dir++) {
+                        res = fmax(res, dfs(grid, i, j, dir, true, 2) + 1);
                     }
                 }
             }
@@ -113,3 +114,7 @@ public:
         return res;
     }
 };
+
+
+
+
