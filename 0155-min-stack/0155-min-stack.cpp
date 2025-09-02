e4ddcfe8651d43dcb1576b0a246db115b848707e
Omit Kumar <111570631+OmitK1302@users.noreply.github.com>
2025-09-02 21:39:32 +0530

    0155-MIN-STACK Stats: Time: 3 ms (61.91%), Space: 23.3 MB (58.06%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,8 +1,56 @@
+// class MinStack {
+// public:
+//     long long minVal = INT_MAX;
+//     stack<long long>st;
+//     // stack<long long>st2;
+//     MinStack() {
+        
+//     }
+    
+//     void push(int val) {
+//         long long currVal = val;
+//         if(val < minVal && !st.empty()) {
+//             currVal = (2 * (long long)val) - minVal;
+//             minVal = val;
+//         }
+
+//         if(st.empty()) minVal = val;
+//         st.push(currVal);
+//         // st2.push(val);
+//     }
+    
+//     void pop() {
+//         if(st.top() < minVal) {
+//             minVal = (2*minVal) - (st.top());
+//         }
+//         st.pop();
+//         // st2.pop();
+//     }
+    
+//     int top() {
+//         return st.top() < minVal ? minVal : st.top(); 
+//     }
+    
+//     int getMin() {
+//         return minVal;
+//     }
+// };
+
+// /**
+//  * Your MinStack object will be instantiated and called as such:
+//  * MinStack* obj = new MinStack();
+//  * obj->push(val);
+//  * obj->pop();
+//  * int param_3 = obj->top();
+//  * int param_4 = obj->getMin();
+//  */
+
+
 class MinStack {
 public:
     long long minVal = INT_MAX;
     stack<long long>st;
-    // stack<long long>st2;
+    stack<long long>st2;
     MinStack() {
         
     }
@@ -16,19 +64,19 @@ public:
 
         if(st.empty()) minVal = val;
         st.push(currVal);
-        // st2.push(val);
+        st2.push(val);
     }
     
     void pop() {
-        if(st.top() < minVal) {
-            minVal = (2*minVal) - (st.top());
+        if(st.top() != st2.top()) {
+            minVal = (2*st2.top()) - (st.top());
         }
         st.pop();
-        // st2.pop();
+        st2.pop();
     }
     
     int top() {
-        return st.top() < minVal ? minVal : st.top(); 
+        return st2.top();
     }
     
     int getMin() {
