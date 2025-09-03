2717383d6033a2b0d7e642fbf4c1c2241a2993e5
Omit Kumar <111570631+OmitK1302@users.noreply.github.com>
2025-09-03 10:26:27 +0530

    3278-FIND-THE-NUMBER-OF-WAYS-TO-PLACE-PEOPLE-I Stats: Time: 0 ms (100%), Space: 32.7 MB (39.13%) - LeetHub

--- 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.cpp
@@ -1,21 +1,46 @@
+class Solution {
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        sort(points.begin(), points.end(), [](vector<int>& a, vector<int>& b) {
+            return a[0] == b[0] ? a[1] > b[1] : a[0] < b [0];
+        });
+
+        int cnt = 0;
+        for(int i = 0; i < points.size()-1; i++) {
+            int top = points[i][1];
+            int low = INT_MIN;
+            for(int j = i+1; j < points.size(); j++) {
+                if(points[j][1] <= top && points[j][1] > low) {
+                    cnt++;
+                    low = points[j][1];
+                }
+            }
+        }
+
+        return cnt;
+    }
+};
+
+// O(n^3)
 // class Solution {
 // public:
 //     int numberOfPairs(vector<vector<int>>& points) {
 //         int res = 0;
-//         // sort(points.begin(), points.end());
-//         if(points.size() == 2 && ((points[0][0] <= points[1][0] && points[0][1] >= points[1][1]) || (points[1][0] <= points[0][0] && points[1][1] >= points[0][1])) ) return 1;
+//         sort(points.begin(), points.end());
 
-//         for(int i = 0; i < points.size()-1; i++) {
+//         for(int i = 0; i < points.size(); i++) {
 //             vector<int>p1 = points[i];
 //             for(int j = 0; j < points.size(); j++) {
 //                 vector<int>p2 = points[j];
 //                 bool isGood = true;
 
-//                 if(j != i && p1[0] <= p2[0] && p1[1] >= p2[1]) {
-
+//                 if(i != j && p1[0] <= p2[0] && p1[1] >= p2[1]) {
+//                     cout<<p1[0]<<" "<<p1[1]<<" "<<p2[0]<<" "<<p2[1]<<endl;
 //                     for(int k = 0; k < points.size(); k++) {
 //                         if(k != i && k != j) {
-//                             if(points[k][0] >= p1[0] && points[k][0] <= p2[0] && points[k][1] <= p1[1] && points[k][1] >= p2[1]) {
+//                             vector<int>p3 = points[k];
+
+//                             if(p3[0] >= p1[0] && p3[0] <= p2[0] && p3[1] <= p1[1] && p3[1] >= p2[1]) {
 //                                 isGood = false;
 //                                 break;
 //                             }
@@ -33,57 +58,3 @@
 //         return res;
 //     }
 // };
-
-
-class Solution {
-public:
-    int numberOfPairs(vector<vector<int>>& points) {
-        int res = 0;
-        sort(points.begin(), points.end());
-
-        for(int i = 0; i < points.size(); i++) {
-            vector<int>p1 = points[i];
-            for(int j = 0; j < points.size(); j++) {
-                vector<int>p2 = points[j];
-                bool isGood = true;
-
-                if(i != j && p1[0] <= p2[0] && p1[1] >= p2[1]) {
-                    cout<<p1[0]<<" "<<p1[1]<<" "<<p2[0]<<" "<<p2[1]<<endl;
-                    // bool left = p1[0] <= p2[0] ? true : false;
-                    // cout<<left<<endl;
-                    // bool left = 1 < 2;
-                    for(int k = 0; k < points.size(); k++) {
-                        if(k != i && k != j) {
-                            vector<int>p3 = points[k];
-
-                            if(p3[0] >= p1[0] && p3[0] <= p2[0] && p3[1] <= p1[1] && p3[1] >= p2[1]) {
-                                // cout<<p3[0]<<" "<<p3[1]<<endl;
-                                isGood = false;
-                                break;
-                            }
-
-
-
-                            // if((left) && (points[k][0] >= points[i][0] && points[k][0] <= points[j][0] && points[k][1] >= points[i][1] && points[k][1] <= points[j][1])) {
-                            //     isGood = false;
-                            //     break;
-                            // }
-                            // if(!(left) && (points[k][0] >= points[j][0] && points[k][0] <= points[i][0] && points[k][1] >= points[j][1] && points[k][1] <= points[i][1])) {
-                            //     isGood = false;
-                            //     break;
-                            // }
-
-                        }
-                    }
-                    if(isGood) {
-                        // cout<<i<<" "<<j<<endl;
-                        res++;
-                    }
-                }
-
-            }
-        }
-
-        return res;
-    }
-};
