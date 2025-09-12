b860853f24f2a5732112527c82b3b976a26b0b2c
Omit Kumar <111570631+OmitK1302@users.noreply.github.com>
2025-09-12 09:18:17 +0530

    3462-VOWELS-GAME-IN-A-STRING Stats: Time: 6 ms (63.25%), Space: 19 MB (53.73%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -4,9 +4,11 @@ public:
         string v = "aeiouAEIOU";
         int cnt = 0;
         for(char& ch : s) {
-            if(v.find(ch) != string::npos) {
-                cnt++;
-            }
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') cnt++;
+ 
+            // if(v.find(ch) != string::npos) {
+            //     cnt++;
+            // }
         }
 
         return cnt == 0 ? false : true;
