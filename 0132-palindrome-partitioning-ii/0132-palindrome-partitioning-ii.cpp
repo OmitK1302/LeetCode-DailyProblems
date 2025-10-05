ca41f6e1fddcbccc77500f7f806b72be9917f324
Omit Kumar <111570631+OmitK1302@users.noreply.github.com>
2025-10-06 00:29:42 +0530

    0132-PALINDROME-PARTITIONING-II Stats: Time: 47 ms (79.05%), Space: 12 MB (48.27%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -2,26 +2,38 @@ class Solution {
 public:
     int minCut(string s) {
         int n = s.size();
-        vector<vector<bool>> isPal(n, vector<bool>(n, false));
-        vector<int> dp(n, 0);
+        vector<vector<bool>>pal(n, vector<bool>(n, false));
 
-        for (int len = 1; len <= n; len++) {
-            for (int i = 0; i + len - 1 < n; i++) {
+        for(int len = 1; len <= n; len++) {
+            for(int i = 0; i < n - len + 1; i++) {
                 int j = i + len - 1;
-                if (s[i] == s[j]) {
-                    if (len <= 2) isPal[i][j] = true;
-                    else isPal[i][j] = isPal[i+1][j-1];
+                if(s[i] == s[j]) {
+                    if(j - i + 1 <= 2) pal[i][j] = true;
+                    else {
+                        pal[i][j] = pal[i+1][j-1];
+                    }
                 }
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            if (isPal[0][i]) {
-                dp[i] = 0;  
-            } else {
+        // for(int i = 0; i < n; i++) {
+        //     for(int j = 0; j < n; j++) {
+        //         cout<<pal[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+
+        
+        // TABULATION PART
+        vector<int>dp(n, 0);
+        for(int i = 0; i < n; i++) {
+            if(pal[0][i]) {
+                dp[i] = 0;
+            }
+            else{
                 dp[i] = INT_MAX;
-                for (int j = 0; j < i; j++) {
-                    if (isPal[j+1][i]) {
+                for(int j = 0; j < i; j++) {
+                    if(pal[j+1][i]) {
                         dp[i] = min(dp[i], dp[j] + 1);
                     }
                 }
